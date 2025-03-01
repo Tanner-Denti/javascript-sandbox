257e0dac1bd011b03c2a71e945cf9576faeed6e6
Tanner Denti <tannerwdenti@gmail.com>
2025-02-28 22:55:58 -0600

    Add styles to sidebar

--- webpack-project/src/scripts/ui/sidebar.js
+++ webpack-project/src/scripts/ui/sidebar.js
@@ -1,8 +1,22 @@
 import "../../styles/sidebar.css";
+import careerImg from "../../images/career.png";
+import genericImg from "../../images/generic.png";
+import groceryImg from "../../images/grocery.png";
+import schoolImg from "../../images/school.png";
+import todayImg from "../../images/today.png";
+import plusImg from "../../images/plus.png";
 import { controller } from "../controller.js";
 
 
 export const sidebar = (function() {
+
+    const _imageMap = {
+        career: careerImg,
+        grocery: groceryImg,
+        school: schoolImg,
+        today: todayImg
+    }
+
     const projects = controller.getProjects();
 
     const sidebarDOM = document.createElement("div");
@@ -10,24 +24,49 @@ export const sidebar = (function() {
 
     const nav = document.createElement("nav");
 
+    const addProjectDiv = document.createElement("div");
+    addProjectDiv.classList.add("add-project-div");
+
+    const plusImgUI = document.createElement("img");
+    plusImgUI.src = plusImg;
+    plusImgUI.alt = "New Project Image";
+    plusImgUI.classList.add("project-icon")
+
     const addProjectBtn = document.createElement("button");
     addProjectBtn.classList.add("project-btn");
     addProjectBtn.innerText = "Add Project";
     addProjectBtn.addEventListener("mouseover", handleMouseOverBtn);
     addProjectBtn.addEventListener("click", handleAddProjectBtnClick);
 
+    addProjectDiv.appendChild(plusImgUI);
+    addProjectDiv.appendChild(addProjectBtn);
+
     loadProjectsNav(nav, projects);
     sidebarDOM.appendChild(nav);
-    sidebarDOM.appendChild(addProjectBtn);
+    sidebarDOM.appendChild(addProjectDiv);
 
     function loadProjectsNav(nav, projects) {
         projects.forEach(proj => {
+            const linkDiv = document.createElement("div");
+            linkDiv.classList.add("project-link-div");
+
+            const projName = proj.getName();
+
+            const img = document.createElement("img");
+            img.src = _imageMap[projName.toLowerCase()] || genericImg;
+            img.alt = `${projName} icon`;
+            img.classList.add("project-icon")
+
             const link = document.createElement("a");
-            link.textContent = proj.getName();
+            link.textContent = projName;
 
             link.addEventListener("mouseover", handleMouseOverLink);
             link.addEventListener("click", handleClickLink);
-            nav.appendChild(link);
+
+            linkDiv.appendChild(img);
+            linkDiv.appendChild(link);
+
+            nav.appendChild(linkDiv);
         });
     }
 
